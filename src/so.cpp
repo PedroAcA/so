6c4cd24764397c7d59f2c97789cfb4a6639df8d3
Roberta Renally da Silva Melo <robertarenallydsm@gmail.com>
2017-10-26 21:14:07 -0200

    2ª versão do modulo de Filas

--- src/so.cpp
+++ src/so.cpp
@@ -15,7 +15,18 @@ http://www.cplusplus.com/reference/deque/deque/
 int main(int argc, char* argv[]){
 	arquivos leitor_arquivos;
 	leitor_arquivos.le_arquivos(argc,argv);
-	filas escalonador;
+	/**
+	//Laco for abaixo eh uma das maneiras de se acessar a lista de processos
+	//Outras formas de acesso a lista (std::vector) podem ser encontradas na 
+	//secao "Element access " de http://www.cplusplus.com/reference/vector/vector/
+	//(acessado em 26/10/2017 as 11:50)
+	for (i = 0; i < (int) leitor_arquivos.lista_processos.size(); i++){
+		//Metodo imprime_infomacoes_processo() espera como argumento de entrada
+		//o PID, que eh simplesmente o (indice atual da lista de processsos +1)
+		leitor_arquivos.lista_processos[i].imprime_infomacoes_processo(i+1);	
+	}
+	**/
+	filas escalonador; 
 	escalonador.insereFilas(leitor_arquivos);
 	escalonador.algoritmoEscalonamento(leitor_arquivos);
 	escalonador.destroiFilas();
